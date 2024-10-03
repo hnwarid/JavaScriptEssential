6b4ea424976ff8bf97e179a4af30825799814ae2
H.N <hn.warid@gmail.com>
2024-10-03 08:00:05 -0400

    add control flow practice task

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -49,3 +49,28 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated
 console.log("Authentication Status:", authenticationStatus);
 
+// Practice task: Dietary services
+
+// let personRole = "Employee";
+let personRole = "Enrolled Member";
+// let personRole = "Subscriber";
+// let personRole = "Non-Subscriber";
+// uncomment one & comment the rest to test
+let accessMessage;
+
+switch (personRole) {
+    case "Enrolled Member":
+        accessMessage = "You have access to Dietary Services and 1-on-1 interaction with a dietician.";
+        break;
+    case "Employee": 
+        accessMessage = "You have access to Dietary Services.";
+        break;
+    case "Subscriber": 
+        accessMessage = "You have partial access to Dietary Services."
+        break;
+    case "Non-Subscriber":
+        accessMessage = "You need to enroll or Subscribe first to access this service."
+        break;
+}
+
+console.log("Person role:", personRole, "\n", "Access Message:", accessMessage);

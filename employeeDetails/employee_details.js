9af40369963c5c49cea34174ab36a4c8664b9eac
H.N <hn.warid@gmail.com>
2024-10-04 13:22:05 -0400

    add practice task find by specialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,9 +1,11 @@
+// declaring the employees list
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... More employee records can be added here
     ];
+
 
 // Function to display all employees
 function displayEmployees() {
@@ -12,11 +14,13 @@ function displayEmployees() {
 
 }
 
+// function to calculate & display the total salaries of all employees
 function calculateTotalSalaries() {
     const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
     alert(`Total salaries: $${totalSalaries}`);
 }
 
+// function to display HR Employees 
 function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department === 'HR');
     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
@@ -30,5 +34,15 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     } else {
         document.getElementById('employeeDetails').innerHTML = 'no employee has been found with this ID';
+    }
+}
+
+// Function to find an employee by Specialization
+function findEmployeeBySpecialization(employeeSpecialization) {
+    const foundEmployee = employees.find(employee => employee.specialization === employeeSpecialization);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - ${foundEmployee.specialization}</p>`;
+    } else {
+        document.getElementById('employeeDetails').innerHTML = 'no employee has been found with this specialization';
     }
 }

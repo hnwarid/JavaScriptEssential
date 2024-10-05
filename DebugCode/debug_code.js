b619f7c96e8495e2e078753dd8b980f227b38a0e
H.N <hn.warid@gmail.com>
2024-10-05 11:29:25 -0400

    add multiple operations drop down

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,10 +3,23 @@ function performOperation() {
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
 
+    // Practice task multiple operation 
+    let operation = document.getElementById('operation').value;
+
     //Check if inputs are valid numbers 
     if (!isNaN(num1) && !isNaN(num2)) {
+        let result;
+
     // Perform the operation
-        let result = multiply(num1, num2);
+        if (operation === 'multiply') {
+            result = multiply(num1, num2)
+        } else if (operation === 'divide') {
+            result = divide(num1, num2)
+        } else if (operation === 'add') {
+            result = add(num1, num2)
+        } else if (operation === 'substract') {
+            result = substract(num1, num2)
+        }
 
         // Display the result 
         displayResult(result);
@@ -23,8 +36,26 @@ function multiply(a, b) {
     return a * b;
 }
 
+// Practice task: multiple operation 
+function divide(a, b) {
+    debugger;
+    return a/b;
+}
+
+function add(a, b) {
+    debugger;
+    return a+b;
+}
+
+function substract(a,b) {
+    debugger;
+    return a-b;
+}
+
 function displayResult(result) {
     // Display the result in the paragraph format
     const resultElement = document.getElementById('result');
     resultElement.textContent = `The result is ${result}`;
 }
+
+

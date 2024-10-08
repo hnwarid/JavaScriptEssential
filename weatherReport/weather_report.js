c3c86f87b4656b4b6437086839f8ecbadf3c03fa
H.N <hn.warid@gmail.com>
2024-10-08 05:54:19 -0400

    add practice task lat & lon input fetch

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,9 +1,34 @@
 function showweatherDetails(event) {
     event.preventDefault();
 
-    const city = document.getElementById('city').value;
+    const cityInput = document.getElementById('city').value;
+    const latInput = document.getElementById('lat').value;
+    const lonInput = document.getElementById('lon').value;
+
     const apiKey = 'c4f86ece00bc8aa272652ac9065af12d';  // supposedly replace with own actual API key, but already works...
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+    // Practice Task: use Latitude and Longitude data to fetch Weather Info
+    // const city = `?q=${cityInput}`;
+    // const API = `&appid=${apiKey}`
+    // const lat = `?lat=${latInput}`;
+    // const lon = `&lon=${lonInput}`;
+    const units = '&units=metric';
+    
+    const mainUrl = `https://api.openweathermap.org/data/2.5/weather`
+    // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = '';
+    // if else logic to create apiUrl from the city name OR latitude and longitude data
+    if (cityInput) {
+        // apiUrl = `${mainUrl}${city}${API}${units}`; 
+        apiUrl = `${mainUrl}?q=${cityInput}&appid=${apiKey}${units}`;
+    } else if (latInput && lonInput) {
+        // apiUrl = `${mainUrl}${lat}${lon}${API}${units}`;
+        apiUrl = `${mainUrl}?lat=${latInput}&lon=${lonInput}&appid=${apiKey}${units}`;
+    } else {
+        alert('Pleaes enter either a city name or both latitude and longitude data');
+        return;
+    }
 
     fetch(apiUrl)
         .then(response => response.json())
